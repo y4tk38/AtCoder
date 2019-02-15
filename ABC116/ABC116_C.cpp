9a71ada65947486e99701ef4ab1ebf568146af77
y4tk38 <yoshitaka19831105@gmail.com>
2019-02-16 08:07:29 +0900

    a

--- ABC116/ABC116_C.cpp
+++ ABC116/ABC116_C.cpp
@@ -5,9 +5,20 @@ int main() {
     int N;
     cin >> N;
   
-    int h[N];
-  
-    for(int i=0; i<N; i++){cin >> h[i];}
+    int h0, h1;
+    int pos_sum = 0;
+    for(int i=0; i<N; i++){
+        if(i==0){
+            cin >> h0;
+            pos_sum += h0;
+        }else{
+            cin >> h1;
+            if(h1-h0>0){pos_sum += h1-h0;}
+            h0 = h1;
+        }
+    }
+
+    cout << pos_sum << endl;
 
     return 0;
 
