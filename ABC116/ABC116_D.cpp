32b68a74053ce674dc78b34f3c4e9c36ee1de615
y4tk38 <yoshitaka19831105@gmail.com>
2019-02-16 09:59:22 +0900

    a

---
+++ ABC116/ABC116_D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N, K;
+    cin >> N >> K;
+  
+    int t[N], d[N];
+
+    for(int i=0; i<N; i++){cin >> t[i] >> d[i];}
+
+    
+
+}
