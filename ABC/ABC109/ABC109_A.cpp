56a833d151fe9cac44c1ea9b5878bdc1bff90455
y4tk38 <yoshitaka19831105@gmail.com>
2019-03-10 19:31:05 +0900

    aaa

---
+++ ABC/ABC109/ABC109_A.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+
+    int A,B;
+    cin >> A >> B;
+
+    if(A%2==1 && B%2==1){
+        cout << "Yes" << endl;
+    } else{cout << "No" << endl;}
+
+    return 0;
+}
